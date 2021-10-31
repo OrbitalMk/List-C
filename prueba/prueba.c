de58c6f944bc13283449b33e74b7748012026972
OrbitalMk <miniorbitalmk@gmail.com>
2021-10-31 13:23:54 -0600

    Nuevo: mejora de error en carpeta prueba

--- prueba/prueba.c
+++ prueba/prueba.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 #include <string.h>
-#include "listadl.h"
+#include "../lista.h"
 
 int main()
 {
-  dlist *p = NULL;
+  list *p = NULL;
 
   p = insert(p, "d", 0);
   p = insert(p, "b", 2);
@@ -14,9 +14,9 @@ int main()
   p = insert(p, "q", 0);
 
   p = delete(p, len(p) - 2, NULL);
-  sort(first(p), 0, len(p) - 1, (int (*)(void *, void *))strcmp);
+  sort(p, 0, len(p) - 1, (int (*)(void *, void *))strcmp);
 
-  for(dlist *temp = p; temp; temp = temp->next)
+  for(list *temp = p; temp; temp = temp->next)
     printf("%s", temp->data);
   printf("\n");
 
--- prueba/pruebad.c
+++ prueba/pruebad.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 #include <string.h>
-#include "lista.h"
+#include "../dlista.h"
 
 int main()
 {
-  list *p = NULL;
+  dlist *p = NULL;
 
   p = insert(p, "d", 0);
   p = insert(p, "b", 2);
@@ -14,9 +14,9 @@ int main()
   p = insert(p, "q", 0);
 
   p = delete(p, len(p) - 2, NULL);
-  sort(p, 0, len(p) - 1, (int (*)(void *, void *))strcmp);
+  sort(first(p), 0, len(p) - 1, (int (*)(void *, void *))strcmp);
 
-  for(list *temp = p; temp; temp = temp->next)
+  for(dlist *temp = p; temp; temp = temp->next)
     printf("%s", temp->data);
   printf("\n");
 
